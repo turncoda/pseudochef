f4c48bb995c33f2aaad1e28530b97db0cdf15fb4
turncoda <144861042+turncoda@users.noreply.github.com>
2026-08-01 01:48:42 -0400

    Add script to bump version

---
+++ scripts/bump_minor_version.bat
@@ -0,0 +1,14 @@
+@echo off
+for /f %%i in ('git status --porcelain') do (
+    echo ERROR: Git working tree is not clean. Commit or stash changes first.
+    exit /b 1
+)
+echo Git tree is clean.
+
+cargo install cargo-edit
+cargo set-version --bump minor
+for /f "tokens=2 delims=#" %%v in ('cargo pkgid') do set "PKG_VER=%%v"
+echo Version is: %PKG_VER%
+git commit -am "v%PKG_VER%"
+git tag "v%PKG_VER%"
+echo Done!
