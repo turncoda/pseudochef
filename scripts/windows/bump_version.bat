739d17930d8ac2648ae2d6cbbd59891a1b625f9a
turncoda <144861042+turncoda@users.noreply.github.com>
2026-08-01 23:57:37 -0400

    Update bump_version to generate initial changelog in commit msg

--- scripts/windows/bump_version.bat
+++ scripts/windows/bump_version.bat
@@ -12,26 +12,46 @@ exit /b 1
 :valid
 
 REM Git tree must be clean.
-
 for /f %%i in ('git status --porcelain') do (
     echo ERROR: Git working tree is not clean. Commit or stash changes first.
     exit /b 1
 )
 echo Git tree is clean.
 
-REM Update Cargo.toml and Cargo.lock.
+REM Get old version.
+for /f "tokens=2 delims=#" %%v in ('cargo pkgid') do set "OLD_VER=%%v"
 
+REM Bump version, update Cargo.toml and Cargo.lock.
 cargo install cargo-edit
 cargo set-version --bump %1
 
 REM Get new version.
+for /f "tokens=2 delims=#" %%v in ('cargo pkgid') do set "NEW_VER=%%v"
 
-for /f "tokens=2 delims=#" %%v in ('cargo pkgid') do set "PKG_VER=%%v"
-echo Version is: %PKG_VER%
+echo Previous version was: %OLD_VER%
+echo New version is: %NEW_VER%
+
+REM Generate changelog using commit messages.
+set "COMMIT_MSG_FILE=%TEMP%\msg.txt"
+(
+REM The ">" is escaped by the "^".
+echo v%OLD_VER% -^> v%NEW_VER%
+echo.
+REM The percent sign needs to be escaped because this is a batch script; it's not for git.
+git log --format="* %%s" --reverse "v%OLD_VER%.."
+) > "%COMMIT_MSG_FILE%"
 
 REM Commit and tag.
+git add -u
+git commit -e -F "%COMMIT_MSG_FILE%"
 
-git commit -am "v%PKG_VER%"
-git tag "v%PKG_VER%"
+REM Reset on abort.
+if %ERRORLEVEL% NEQ 0 (
+    echo Aborting!
+    git reset --hard HEAD
+    exit /b 1
+)
 
-echo Done!
+REM Commit was successful; proceed to tag.
+git tag "v%NEW_VER%"
+echo Created tag: v%NEW_VER%
