0c741e0d6b7bbaa9378ca8bf1572197234a1a829
turncoda <144861042+turncoda@users.noreply.github.com>
2026-08-15 01:41:17 -0400

    Bundle metadata for textures from game
    Part of #10

--- src/texture_collection.rs
+++ src/texture_collection.rs
@@ -41,10 +41,339 @@ impl TextureCollection {
     pub fn bundled() -> Self {
         let mut m: HashMap<&'static str, Texture> = HashMap::new();
         m.insert(
+            "MatTex/Textures/Environment/Cave_Brick",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/Red_floor",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/Theater_Floor",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/artDecoPattern3",
+            Texture {
+                width: 64,
+                height: 62,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_door_window",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_floor_01",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_02",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_03",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_04",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_05",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_06",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_07",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/building_wall_08",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/roofShingles",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/weirdWall",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/Environment/wooden_boards",
+            Texture {
+                width: 64,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/T_FluidWater",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/T_WaterSurface",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
             "MatTex/Textures/big_brick",
             Texture {
                 width: 64,
                 height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/cave_brick",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/cavetexture",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/checkerTile",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/fence",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/marble_texture",
+            Texture {
+                width: 64,
+                height: 64,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0005",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0008",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0009",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0010",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0011",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0012",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0013",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0014",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0015",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0016",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0017",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0018",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0023",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0024",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0025",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moon0026",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0000",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0003",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0005",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0006",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0007",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0008",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0010",
+            Texture {
+                width: 32,
+                height: 32,
+            },
+        );
+        m.insert(
+            "MatTex/Textures/moontexture0011",
+            Texture {
+                width: 32,
+                height: 32,
             },
         );
         Self { m: Some(m) }
